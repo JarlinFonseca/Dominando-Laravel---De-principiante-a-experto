2b1271903c2dc351d3f3c3d6167d02bc10527088
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2022-09-05 18:45:57 -0500

    Laravel Mix, Diseño Bootstrap parte 1

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,50 +5,99 @@
 
 @section('content')
 
-<h1>{{ __('Contact') }}</h1>
+<div class="container">
 
-{{-- @if($errors->any())
+    {{-- .row>.col-12 --}}
 
-        {{ var_dump($errors->all()) }} 
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
 
-        <ul>
-            @foreach ($errors->all() as $error )
-            <li>{{ $error }}</li>
+
+
+            {{-- @if($errors->any())
             
-        @endforeach
-        </ul>
-      
-@endif
- --}}
-{{-- Ver errores --}}
-{{-- {{ var_dump($errors->any()) }} --}}
+                    {{ var_dump($errors->all()) }} 
+            
+                    <ul>
+                        @foreach ($errors->all() as $error )
+                        <li>{{ $error }}</li>
+                        
+                    @endforeach
+                    </ul>
+                  
+            @endif
+             --}}
+            {{-- Ver errores --}}
+            {{-- {{ var_dump($errors->any()) }} --}}
+            
+            
+                <form  class="bg-white shadow rounded py-3 px-4" action="{{ route('messages.store') }}" method="POST">
+                    @csrf
+                    <h1 class="display-4">{{ __('Contact') }}</h1>
+                    
+                    <div class="form-group">
+                        <label for="name">Nombre</label>
+                        <input class="form-control bg-light shadow-sm  
+                        @error('name') is-invalid  @else border-0 @enderror"
+                        type="text" id="name" name="name" placeholder="Nombre..."  value="{{ old('name') }}">
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                           {{--  {!! $errors->first('name', '<small style="color:red;">:message</small><br>') !!} --}}
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="email">Correo electrónico</label>
+                        <input class="form-control bg-light shadow-sm  
+                        @error('email') is-invalid  @else border-0 @enderror"
+                        type="text" id="email" name="email" placeholder="Email..."  value="{{ old('email') }}">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="subject">Asunto</label>
+                        <input class="form-control bg-light shadow-sm  
+                        @error('subject') is-invalid  @else border-0 @enderror"
+                        type="text" id="subject" name="subject" placeholder="Asunto..."  value="{{ old('subject') }}">
+                        @error('subject')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+        
+                    <div class="form-group">
+                        <label for="content">Contenido</label>
+                        <textarea class="form-control bg-light shadow-sm  
+                        @error('content') is-invalid  @else border-0 @enderror"
+                        type="text" id="subject" name="content" placeholder="Mensaje..." > {{ old('content') }}</textarea>
+                        @error('content')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+          
+                    
+                    <button class="btn btn-primary btn-lg btn-block">@lang('Send')</button>
+                </form>
+        
+        
 
 
-    <form action="{{ route('messages.store') }}" method="POST">
-        @csrf
-        
-        <label for="name">Digite su nombre:</label>
-        <p>
-            <input type="text" name="name" placeholder="Nombre..."  value="{{ old('name') }}"><br>
-            {!! $errors->first('name', '<small style="color:red;">:message</small><br>') !!}
-        </p>
-        <label for="email">Digite correo al cúal enviara la información:</label>
-        <P>
-        
-            <input type="text" name="email" placeholder="Email..." value="{{ old('email') }}"><br>
-            {!! $errors->first('email', '<small style="color:red;">:message</small><br>') !!}
-        </P>
-        <label for="name">Digite el asunto del correo:</label>
-        <P>
-            <input type="text" name="subject" placeholder="Asunto..." value="{{ old('subject') }}"><br>
-            {!! $errors->first('subject', '<small style="color:red;">:message</small><br>') !!}
-        </P>
-        <label for="name">Digite su mensaje:</label>
-        <P>
-        <textarea name="content" placeholder="Mensaje....">{{old('content')}}</textarea><br>
-        {!! $errors->first('content', '<small style="color:red;">:message</small><br>') !!}
-        </P>
-        
-        <button>@lang('Send')</button>
-    </form>
+        </div>
+    </div>
+
+
+
+
+</div>
+
+
 @endsection
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,21 @@
-<nav>
+<nav class="navbar navbar-light navbar-expand-lg  bg-white shadow-sm">
+
+
+    <div class="container">
+
+    <a class="navbar-brand" href="{{ route('home') }}">
+        {{ config('app.name') }}
+    </a>
+
+
+    <button class="navbar-toggler" type="button" 
+    data-toggle="collapse"
+    data-target="#navbarSupportedContent"
+     aria-controls="navbarSupportedContent" 
+     aria-expanded="false"
+     aria-label="{{ __('Toggle navigation') }}">
+     <span class="navbar-toggler-icon"></span>
+ </button>
         
     {{-- Ver URL --}}
     {{-- {{ dump(request()->url() )}} --}}
@@ -7,25 +24,31 @@
     {{-- Comparar si estamos sobre la ruta --}}
     {{-- {{ dump(request()->routeIs('portfolio.index') )}} --}}
 
-    <ul>
-        <li class="{{ setActive('home') }}"><a href="{{ route('home') }}">@lang('Home')</a></li>
-        <li class="{{ setActive('about') }}"><a href="{{ route('about') }}">@lang('About')</a></li>
-        <li class="{{ setActive('projects.*') }}"><a href="{{ route('projects.index') }}">@lang('Projects')</a></li>
-        <li class="{{ setActive('contact') }}"><a href="{{ route('contact') }}">@lang('Contact')</a></li>
-       
-        {{-- Invitado, si no ha iniciado sesión muestra el link del Login sino lo oculta 
-            Si no hay un usuario autenticado mostramos el link del login , caso contrario;
-            en el caso de que este autenticado mostrar el link para cerrar sesión.
-            --}}
-        @guest
-            <li><a href="{{ route('login') }}">Login</a></li>
-        @else
-            <li>
-                <a href="#"  onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">Cerrar sesión</a>
-            </li>
-        @endguest
-    </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a class="nav-link {{ setActive('home') }}" href="{{ route('home') }}">@lang('Home')</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('about') }}" href="{{ route('about') }}">@lang('About')</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('projects.*') }}" href="{{ route('projects.index') }}">@lang('Projects')</a></li>
+            <li class="nav-item"><a class="nav-link {{ setActive('contact') }}" href="{{ route('contact') }}">@lang('Contact')</a></li>
+           
+            {{-- Invitado, si no ha iniciado sesión muestra el link del Login sino lo oculta 
+                Si no hay un usuario autenticado mostramos el link del login , caso contrario;
+                en el caso de que este autenticado mostrar el link para cerrar sesión.
+                --}}
+            @guest
+                <li class="nav-item"><a class="nav-link {{ setActive('login')}}" href="{{ route('login') }}">Login</a></li>
+            @else
+                <li>
+                    <a href="#"  onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">Cerrar sesión</a>
+                </li>
+            @endguest
+        </ul>
+    </div>
+
+</div>
+
+
 </nav>
 
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
